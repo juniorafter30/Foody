4a59ef527ffda16874ceda846dbc128ad284f8bb
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-03-17 14:39:11 +0100

    changes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,12 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
+const showMore = document.querySelector("#showMore");
+const menuMore = document.querySelector("#menuMore");
 
 burger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
+});
+
+showMore.addEventListener("click", () => {
+  menuMore.classList.toggle("hidden");
 });
